ac9e48fbc18e983d809ad6a144900193bf6f0b3c
Ferina Aqila <ferinaaql@gmail.com>
2021-01-05 01:58:40 +0700

    tugas mvc

---
+++ cobamvc/src/cobamvc/MVC_Praktikum.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cobamvc;
+
+/**
+ *
+ * @author ASUS
+ */
+public class MVC_Praktikum {
+    ViewPraktikum vp = new ViewPraktikum();
+    ModelPraktikum mp = new ModelPraktikum();
+    ControllerPraktikum cp = new ControllerPraktikum(vp, mp);
+}
